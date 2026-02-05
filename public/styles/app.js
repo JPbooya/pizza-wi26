a2b663a12a59204b3eefb799b97d1865df6148f1
JPbooya <perezjcarlos81@gmail.com>
2026-02-05 13:28:37 -0800

    added files

---
+++ public/styles/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${import.meta.dirname}/views/home.html`)
+});
+
+// Contact route
+app.get('/contact-us', (req, res) => {
+  res.sendFile(`${import.meta.dirname}/views/contact.html`)
+});
+
+
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost${3000}`)
+});
+
